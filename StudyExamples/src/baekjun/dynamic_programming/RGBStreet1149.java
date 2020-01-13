0bee7e0fc331c21879950909d7efcb58d10ad42b
kwanik-kor <kwanigi2005@gmail.com>
2020-01-13 14:50:53 +0900

    20200113 14:50 백준알고리즘/1149/RGB거리

---
+++ StudyExamples/src/baekjun/dynamic_programming/RGBStreet1149.java
@@ -0,0 +1,29 @@
+package baekjun.dynamic_programming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class RGBStreet1149 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int dp[][] = new int[N][3];
+		StringTokenizer st;
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			dp[i][0] = Integer.parseInt(st.nextToken());
+			dp[i][1] = Integer.parseInt(st.nextToken());
+			dp[i][2] = Integer.parseInt(st.nextToken());
+		}
+		for(int i = 1; i<N; i++) {
+			dp[i][0] += Math.min(dp[i-1][1], dp[i-1][2]);
+			dp[i][1] += Math.min(dp[i-1][0], dp[i-1][2]);
+			dp[i][2] += Math.min(dp[i-1][0], dp[i-1][1]);
+		}
+		System.out.println(Math.min(Math.min(dp[N-1][0], dp[N-1][1]), dp[N-1][2]));
+	}
+
+}
