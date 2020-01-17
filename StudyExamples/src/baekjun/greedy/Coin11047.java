6379d14deaea0b5ce186ca073d66ce08913f8a82
kwanik-kor <kwanigi2005@gmail.com>
2020-01-17 20:34:07 +0900

    20200117 20:33 백준알고리즘/11047/동전 0

---
+++ StudyExamples/src/baekjun/greedy/Coin11047.java
@@ -0,0 +1,35 @@
+package baekjun.greedy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Coin11047 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int arr[] = new int[N];
+		for(int i = 0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		int cnt = 0;
+		for(int i = N-1; i>=0; i--) {
+			if(arr[i] <= K) {
+				K -= arr[i];
+				cnt++;
+				i++;
+			}
+		}
+		bw.write(String.valueOf(cnt));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
