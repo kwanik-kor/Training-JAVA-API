141281b3ac476ff48116ba923127712d8b2a021f
kwanik-kor <kwanigi2005@gmail.com>
2020-01-18 13:05:22 +0900

    20200118 13:05 백준알고리즘/4679/The Snail

---
+++ StudyExamples/src/baekjun/Snail4679.java
@@ -0,0 +1,40 @@
+package baekjun;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Snail4679 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		while(true) {
+			int H = sc.nextInt();
+			if(H == 0) break;
+			float U = sc.nextInt();
+			int D = sc.nextInt();
+			int F = sc.nextInt();
+			int day = 0;
+			float sum = 0f;
+			float fa = U*F/100f;
+			if(U <= H) {
+				day++;
+				sum = U - D;
+				while(sum <= H) {
+					day++;
+					U -= fa;
+					sum += U;
+					if(sum > H) break;
+					sum -= D;
+					if(sum < 0) break;
+				}
+			}
+			bw.write((sum < H) ? String.format("failure on day %d\n", day) : String.format("success on day %d\n", day));
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
