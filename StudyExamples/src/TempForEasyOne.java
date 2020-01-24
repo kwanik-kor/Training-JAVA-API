b90ef139aab5b09bded2452ea4bd9dd469eee59f
kwanik-kor <kwanigi2005@gmail.com>
2020-01-24 16:21:43 +0900

    20200124 16:21 백준알고리즘/A+B-6

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,25 +3,15 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		for(int t = 0; t<3; t++) {
-			st = new StringTokenizer(br.readLine());
-			int cnt = 0;
-			for(int i = 0; i<4; i++)
-				cnt = (Integer.parseInt(st.nextToken()) == 0)? cnt + 1 : cnt;
-			switch(cnt) {
-			case 0: bw.write("E\n"); break;
-			case 1: bw.write("A\n"); break;
-			case 2: bw.write("B\n"); break;
-			case 3: bw.write("C\n"); break;
-			case 4: bw.write("D\n"); break;
-			}
+		int T = Integer.parseInt(br.readLine());
+		for(int t = 0; t<T; t++) {
+			String s[] = br.readLine().split(",");
+			bw.write(Integer.parseInt(s[0]) + Integer.parseInt(s[1]) + "\n");
 		}
 		bw.flush();
 		bw.close();
