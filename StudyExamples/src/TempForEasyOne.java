52355c4e028515629df9508b0f6bf7d44fd20415
kwanik-kor <kwanigi2005@gmail.com>
2020-01-27 21:15:06 +0900

    20200127 21:14 백준알고리즘/4999/아!

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -8,7 +8,7 @@ public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(br.readLine().length()+"");
+		bw.write((br.readLine().length() >= br.readLine().length())? "go" : "no");
 		bw.flush();
 		bw.close();
 		br.close();
