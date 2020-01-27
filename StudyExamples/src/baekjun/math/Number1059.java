fb01b2ed35c80aa14dd8556384dd57100d0532b3
kwanik-kor <kwanigi2005@gmail.com>
2020-01-27 23:39:49 +0900

    20200127 23:40 백준알고리즘/3047/ABC

--- StudyExamples/src/baekjun/math/Number1059.java
+++ StudyExamples/src/baekjun/math/Number1059.java
@@ -1,3 +1,5 @@
+package baekjun.math;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -6,21 +8,37 @@ import java.io.OutputStreamWriter;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class TempForEasyOne {
+public class Number1059 {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int L = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int arr[] = new int[3];
-		for(int i = 0; i<3; i++)
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[L];
+		for(int i = 0; i<L; i++)
 			arr[i] = Integer.parseInt(st.nextToken());
 		Arrays.sort(arr);
-		String s = br.readLine();
-		for(int i = 0; i<3; i++) {
-			bw.write(arr[(int)s.charAt(i)-65] + " ");
+		int a = 0;
+		int b = 0;
+		for(int i = 0; i<L-1; i++) {
+			if(arr[i] < N && arr[i+1] > N) {
+				a = arr[i];
+				b = arr[i+1];
+				break;
+			}
 		}
+		int result = 0;
+		if(a != 0 && b != 0) {
+			result = (N-b-1)*(a-N) + (a-N-1);
+		}
+		
+		bw.write(result + "");
 		bw.flush();
 		bw.close();
 		br.close();
+			
 	}
+
 }
