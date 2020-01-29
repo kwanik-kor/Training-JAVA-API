c52ce1862ebc6af386aad9d95d6979727fe0bc87
kwanik-kor <kwanigi2005@gmail.com>
2020-01-29 21:25:13 +0900

    20200129 21:25 백준알고리즘/10867/중복없이 정렬

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,22 +3,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int arr[] = new int[3];
-		for(int i = 0; i<3; i++)
-			arr[i] = Integer.parseInt(st.nextToken());
-		Arrays.sort(arr);
-		String s = br.readLine();
-		for(int i = 0; i<3; i++) {
-			bw.write(arr[(int)s.charAt(i)-65] + " ");
-		}
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i = 0; i<N; i++)
+			list.add(Integer.parseInt(st.nextToken()));
+		HashSet<Integer> temp = new HashSet<>(list);
+		list = new ArrayList<>(temp);
+		Collections.sort(list);
+		for(Integer i : list)
+			bw.write(i + " ");
 		bw.flush();
 		bw.close();
 		br.close();
