16ed20880ae0aba2d20afaf166d79eceba87fb62
kwanik-kor <kwanigi2005@gmail.com>
2020-02-22 11:14:41 +0900

    20200222 11:14 백준알고리즘/9252/LCS - 2

--- StudyExamples/src/baekjun/dynamic_programming/LCS9252.java
+++ StudyExamples/src/baekjun/dynamic_programming/LCS9252.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class LCS9252 {
 	static int dp[][];
@@ -16,14 +17,37 @@ public class LCS9252 {
 		String b = br.readLine();
 		dp = new int[a.length()+1][b.length()+1];
 		for(int i = 1; i<=a.length(); i++) {
-			for(int j = 1; j<=a.length(); j++) {
+			for(int j = 1; j<=b.length(); j++) {
 				if(a.charAt(i-1) == b.charAt(j-1))
 					dp[i][j] = dp[i-1][j-1] + 1;
 				else
 					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
 			}
 		}
-		System.out.println(dp[a.length()][b.length()]);
+		int result = dp[a.length()][b.length()];
+		bw.write(result + "\n");
+		
+		int x = a.length();
+		int y = b.length();
+		Stack<Integer> stack = new Stack<>();
+		while(dp[x][y] != 0) {
+			if(dp[x-1][y] == result) {
+				x--;
+				continue;
+			}
+			if(dp[x][y-1] == result) {
+				y--;
+				continue;
+			}
+			stack.push(x);
+			x--;
+			y--;
+			result--;
+		}
+		while(!stack.isEmpty())
+			bw.write(a.charAt(stack.pop()-1) + "");
+		bw.flush();
+		bw.close();
+		br.close();
 	}
-
 }
