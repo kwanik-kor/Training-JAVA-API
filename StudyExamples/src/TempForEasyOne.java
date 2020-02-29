372301b97c54ef7b86b4cd2b2b44dab15f3ce67e
kwanik-kor <kwanigi2005@gmail.com>
2020-02-29 13:53:15 +0900

    20200229 13:52 백준알고리즘/1269/대칭 차집합

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,22 +3,28 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int Y = 0;
-		int M = 0;
-		for(int i = 0; i<N; i++) {
-			int time = Integer.parseInt(st.nextToken());
-			Y += ((time/30) + 1) * 10;
-			M += ((time/60) + 1) * 15;
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		HashSet<Integer> set = new HashSet<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<N; i++)
+			set.add(Integer.parseInt(st.nextToken()));
+		st = new StringTokenizer(br.readLine());
+		int cnt = 0;
+		for(int i = 0; i<M; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			if(set.contains(n)) cnt++;
+			set.add(n);
 		}
-		bw.write((Y < M)? "Y " + Y : ((M < Y)? "M " + M : "Y M " + Y));
+		bw.write(set.size() - cnt + "");
 		bw.flush();
 		bw.close();
 		br.close();
