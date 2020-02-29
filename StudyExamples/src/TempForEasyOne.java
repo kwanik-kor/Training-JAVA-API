4c029472d4377f9a9aa5a9f4aea0227757de9f34
kwanik-kor <kwanigi2005@gmail.com>
2020-02-29 15:39:06 +0900

    20200229 15:38 백준알고리즘/1598/꼬리를 무는 숫자 나열

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,21 +3,15 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String hexa = br.readLine();
-		int ans = 0;
-		int ten = 1;
-		for(int i = hexa.length() - 1; i>=0; i--) {
-			char c = hexa.charAt(i);
-			int val = (c >= 65)? c - 55 : c - '0';
-			ans += val*ten;
-			ten *= 16;
-		}
-		bw.write(ans + "");
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
 		bw.flush();
 		bw.close();
 		br.close();
