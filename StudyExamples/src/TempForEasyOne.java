ffd9342546f8bd0226f1167454c628eef040fc48
kwanik-kor <kwanigi2005@gmail.com>
2020-02-29 12:57:45 +0900

    20200229 12:57 백준알고리즘/1264/모음의개수

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,25 +3,21 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		ArrayList<Integer> list = new ArrayList<>();
-		for(int i = 0; i<N; i++)
-			list.add(Integer.parseInt(st.nextToken()));
-		HashSet<Integer> temp = new HashSet<>(list);
-		list = new ArrayList<>(temp);
-		Collections.sort(list);
-		for(Integer i : list)
-			bw.write(i + " ");
+		String s = "";
+		while(!(s = br.readLine()).equals("#")) {
+			int cnt = 0;
+			for(int i = 0; i<s.length(); i++) {
+				char c = s.charAt(i);
+				if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+					cnt++;
+			}
+			bw.write(cnt+"\n");
+		}
 		bw.flush();
 		bw.close();
 		br.close();
