cd15dc8ed0d9f62aa52d2838757ca808dc3bab63
kwanik-kor <kwanigi2005@gmail.com>
2020-03-01 22:18:38 +0900

    20200301 22:18 백준알고리즘/1748/수 이어 쓰기1

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,15 +3,23 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
+		int total = 0;
+		int ten = 10;
+		int length = 1;
+		for(int i = 1; i<=N; i++) {
+			if(i == ten) {
+				length++;
+				ten *= 10;
+			}
+			total += length;
+		}
+		bw.write(total + "");
 		bw.flush();
 		bw.close();
 		br.close();
