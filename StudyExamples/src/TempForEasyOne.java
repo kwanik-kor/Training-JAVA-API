cad05109bee4d84fa827c1da29529207ff49b6a5
kwanik-kor <kwanigi2005@gmail.com>
2020-03-02 13:43:12 +0900

    20200302 13:45 백준알고리즘/2480/주사위 세개

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,29 +3,23 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-		int max = 0;
-		StringTokenizer st;
-		for(int i = 0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			int price = 0;
-			if(a == b && b == c) price = 10000 + 1000*a;
-			else if(a == b) price = 1000 + 100*a;
-			else if(b == c) price = 1000 + 100*b;
-			else if(c == a) price = 1000 + 100*c;
-			else price = 100*Math.max(Math.max(a, b), c);
-			max = Math.max(max, price);
-		}
-		bw.write(max + "");
+		int dice[] = new int[3];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<3; i++)
+			dice[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(dice);
+		if(dice[0] == dice[1] && dice[1] == dice[2])
+			bw.write(10000 + dice[0]*1000 + "");
+		else if(dice[0] == dice[1] || dice[1] == dice[2])
+			bw.write(1000 + dice[1]*100 + "");
+		else bw.write(dice[2]*100 + "");
 		bw.flush();
 		bw.close();
 		br.close();
