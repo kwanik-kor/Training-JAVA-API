7189028bebe7a2fe0a39a7c9f270061f1055ecd8
kwanik-kor <kwanigi2005@gmail.com>
2020-03-02 12:40:42 +0900

    20200302 12:40 백준알고리즘/13163/닉네임에 갓 붙이기

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,20 +3,22 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String s = "";
-		while(!(s = br.readLine()).equals("#")) {
-			char c = s.charAt(0);
-			char arr[] = s.substring(2).toCharArray();
-			int cnt = 0;
-			for(int i = 0; i<arr.length; i++) {
-				if(arr[i] == c || arr[i] == c - 32) cnt++;
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			st.nextToken();
+			bw.write("god");
+			while(st.hasMoreTokens()) {
+				bw.write(st.nextToken());
 			}
-			bw.write(c + " " + cnt + "\n");
+			bw.write("\n");
 		}
 		bw.flush();
 		bw.close();
