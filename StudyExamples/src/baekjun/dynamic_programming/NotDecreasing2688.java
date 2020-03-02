06bc828e8157f49daff77ec15c1a317a8519c863
kwanik-kor <kwanigi2005@gmail.com>
2020-03-02 17:23:32 +0900

    20200302 17:23 백준알고리즘/2688/줄어들지 않아

--- StudyExamples/src/baekjun/dynamic_programming/NotDecreasing2688.java
+++ StudyExamples/src/baekjun/dynamic_programming/NotDecreasing2688.java
@@ -1,30 +1,33 @@
+package baekjun.dynamic_programming;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class TempForEasyOne {
+public class NotDecreasing2688 {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int testCase = Integer.parseInt(br.readLine());
-		String cases[] = {"TTT", "TTH", "THT", "THH", "HTT", "HTH", "HHT", "HHH"};
-		for(int t = 0; t<testCase; t++) {
-			int arr[] = new int[8];
-			String s = br.readLine();
-			for(int i = 0; i<38; i++) {
-				for(int j = 0; j<8; j++) {
-					if(s.substring(i, i+3).equals(cases[j]))
-						arr[j]++;
-				}
+		long dp[][] = new long[65][10];
+		for(int i = 0; i<10; i++) 
+			dp[1][i] = i+1;
+		for(int i = 2; i<65; i++) {
+			dp[i][0] = 1;
+			for(int j = 1; j<10; j++) {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1];
 			}
-			for(int i = 0; i<8; i++)
-				bw.write(arr[i] + " ");
-			bw.write("\n");
+		}
+		for(int t = 0; t<testCase; t++) {
+			int n = Integer.parseInt(br.readLine());
+			bw.write(dp[n][9]+ "\n");
 		}
 		bw.flush();
 		bw.close();
 		br.close();
 	}
+
 }
