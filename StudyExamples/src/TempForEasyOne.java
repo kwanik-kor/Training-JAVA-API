4041357e88e261cc6f3dd4f1386f2274838cacd5
kwanik-kor <kwanigi2005@gmail.com>
2020-03-02 13:35:41 +0900

    20200302 13:35 백준알고리즘/2476/주사위 게임

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -9,13 +9,23 @@ public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int sum = 0;
-		for(int i = 0; i<5; i++) {
-			int n = Integer.parseInt(st.nextToken());
-			sum += n*n%10;
+		int N = Integer.parseInt(br.readLine());
+		int max = 0;
+		StringTokenizer st;
+		for(int i = 0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int price = 0;
+			if(a == b && b == c) price = 10000 + 1000*a;
+			else if(a == b) price = 1000 + 100*a;
+			else if(b == c) price = 1000 + 100*b;
+			else if(c == a) price = 1000 + 100*c;
+			else price = 100*Math.max(Math.max(a, b), c);
+			max = Math.max(max, price);
 		}
-		bw.write(sum%10 + "");
+		bw.write(max + "");
 		bw.flush();
 		bw.close();
 		br.close();
