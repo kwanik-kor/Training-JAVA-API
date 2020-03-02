db50a69d3a04cacc87c0838f37e4de7564ce0484
kwanik-kor <kwanigi2005@gmail.com>
2020-03-02 15:34:54 +0900

    20200302 15:35 백준알고리즘/2506/점수계산

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -11,18 +11,17 @@ public class TempForEasyOne {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int arr[] = new int[N+1];
-		arr[0] = 500001;
-		for(int i = 1; i<=N; i++) {
-			int n = Integer.parseInt(st.nextToken());
-			arr[i] = n;
-			for(int j = i-1; j>=0; j--) {
-				if(arr[j] >= n) {
-					bw.write(j + " ");
-					break;
-				}
+		int flag = 0;
+		int total = 0;
+		for(int i = 0; i<N; i++) {
+			int ox = Integer.parseInt(st.nextToken());
+			if(ox == 0) flag = 0;
+			else {
+				flag++;
+				total += flag;
 			}
 		}
+		bw.write(total+"");
 		bw.flush();
 		bw.close();
 		br.close();
