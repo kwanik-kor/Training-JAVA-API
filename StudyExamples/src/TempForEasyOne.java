6a0d67e3ef074a5a15297ce99ed2fcfa5556c3a6
kwanik-kor <kwanigi2005@gmail.com>
2020-03-03 10:29:09 +0900

    20200303 10:28 백준알고리즘/2914/저작권

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,30 +3,16 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int K = Integer.parseInt(br.readLine());
-		int N = 1;
-		while(K > N) {
-			N *= 2;
-		}
-		bw.write(N + " ");
-		if(K == N) bw.write(0 + "");
-		else {
-			int cnt = 0;
-			while(K != 0) {
-				if(K >= N)
-					K -= N;
-				else {
-					N/=2;
-					cnt++;
-				}
-			}
-			bw.write(cnt + "");
-		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int I = Integer.parseInt(st.nextToken());
+		bw.write(A*(I-1) + 1 + "");
 		bw.flush();
 		bw.close();
 		br.close();
