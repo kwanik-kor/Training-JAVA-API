733c7bae0162b48aa5a953215f2e381eedb1cea6
kwanik-kor <kwanigi2005@gmail.com>
2020-03-04 20:45:53 +0900

    20200304 20:45 백준알고리즘/4150/피보나치 수

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,19 +3,24 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.math.BigInteger;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		String s = "";
-		while(!(s = br.readLine()).equals("0 0")) {
-			StringTokenizer st = new StringTokenizer(s);
-			bw.write((Integer.parseInt(st.nextToken()) > Integer.parseInt(st.nextToken()))? "Yes\n" : "No\n");
+		int N = Integer.parseInt(br.readLine());
+		BigInteger a = BigInteger.valueOf(1);
+		BigInteger b = BigInteger.valueOf(1);
+		for(int i = 2; i<=N; i++) {
+			BigInteger c = a.add(b);
+			a = b;
+			b = c;
 		}
+		bw.write(a.toString());
 		bw.flush();
 		bw.close();
 		br.close();
 	}
+	
 }
