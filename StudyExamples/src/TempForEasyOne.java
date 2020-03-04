5c39ae16a1ede94314e8a03cc68147e6a5c2230e
kwanik-kor <kwanigi2005@gmail.com>
2020-03-04 20:20:13 +0900

    20200304 20:20 백준알고리즘/4101/크냐?

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -9,30 +9,11 @@ public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		boolean map[][] = new boolean[M][N];
-		int x[] = {0, 1, 0, -1};
-		int y[] = {1, 0, -1, 0};
-		int dx = 0, dy = 0;
-		int cnt = 0;
-		int flag = 0;
-		while(true) {
-			if(map[dx][dy]) break;
-			map[dx][dy] = true;
-			int nx = dx + x[flag];
-			int ny = dy + y[flag];
-			if(nx < 0 || ny < 0 || nx >= M || ny >= N || map[nx][ny]) {
-				flag = (flag+1)%4;
-				nx = dx + x[flag];
-				ny = dy + y[flag];
-				cnt++;
-			}
-			dx = nx;
-			dy = ny;
+		String s = "";
+		while(!(s = br.readLine()).equals("0 0")) {
+			StringTokenizer st = new StringTokenizer(s);
+			bw.write((Integer.parseInt(st.nextToken()) > Integer.parseInt(st.nextToken()))? "Yes\n" : "No\n");
 		}
-		bw.write(cnt-1 + "");
 		bw.flush();
 		bw.close();
 		br.close();
