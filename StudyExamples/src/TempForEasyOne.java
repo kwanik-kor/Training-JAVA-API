ddb7b66e6ab8fdd6856eab3038f85cee9e92177e
kwanik-kor <kwanigi2005@gmail.com>
2020-03-04 22:16:57 +0900

    20200304 22:16 백준알고리즘/4287/Word Ratios

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,21 +3,28 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(br.readLine());
-		BigInteger a = BigInteger.valueOf(1);
-		BigInteger b = BigInteger.valueOf(1);
-		for(int i = 2; i<=N; i++) {
-			BigInteger c = a.add(b);
-			a = b;
-			b = c;
+		String s = "";
+		while(!(s = br.readLine()).equals("#")) {
+			StringTokenizer st = new StringTokenizer(s);
+			String a = st.nextToken();
+			String b = st.nextToken();
+			String c = st.nextToken();
+			char arr[] = a.toCharArray();
+			char brr[] = b.toCharArray();
+			char crr[] = c.toCharArray();
+			StringBuilder sb = new StringBuilder("");
+			for(int i = 0; i<arr.length; i++) {
+				int n = (brr[i] - arr[i] > 0)? brr[i] - arr[i] : 26 - (arr[i]-'a') + (brr[i]-'a');
+				sb.append((char)((crr[i]-'a'+n)%26 + 'a'));
+			}
+			bw.write(a + " " + b + " " + c + " " + sb.toString() + "\n");
 		}
-		bw.write(a.toString());
 		bw.flush();
 		bw.close();
 		br.close();
