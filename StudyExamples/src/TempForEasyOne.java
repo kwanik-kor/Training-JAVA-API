e82e65cbc7719fad1e90b901646dd1dc5b1985a9
kwanik-kor <kwanigi2005@gmail.com>
2020-03-04 19:11:39 +0900

    20200304 19:11 백준알고리즘/3023/마술사 이민혁

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -9,10 +9,35 @@ public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int chess[] = {1, 1, 2, 2, 2, 8};
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i<6; i++)
-			bw.write(chess[i] - Integer.parseInt(st.nextToken()) + " ");
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		char card[][] = new char[2*R][2*C];
+		for(int i = 0; i<R; i++) {
+			String tmp = br.readLine();
+			for(int j = 0; j<C; j++)
+				card[i][j] = tmp.charAt(j);
+		}
+		for(int i = 0; i<R; i++) {
+			for(int j = C; j<2*C; j++)
+				card[i][j] = card[i][2*C - j - 1];
+		}
+		for(int i = R; i<2*R; i++) {
+			for(int j = 0; j<2*C; j++)
+				card[i][j] = card[2*R - i -1][j];
+		}
+		st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken()) - 1;
+		int B = Integer.parseInt(st.nextToken()) - 1;
+		for(int i = 0; i<2*R; i++) {
+			for(int j = 0; j<2*C; j++) {
+				if(i == A && j == B) {
+					if(card[i][j] == '#') bw.write(".");
+					else bw.write("#");
+				}else bw.write(card[i][j] + "");
+			}
+			bw.write("\n");
+		}
 		bw.flush();
 		bw.close();
 		br.close();
