9a9ee592c006bd551dbec994061f048aa0d5fcd8
kwanik-kor <kwanigi2005@gmail.com>
2020-03-04 10:38:21 +0900

    2020304 10:38 백준알고리즘/2998/8진수

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,24 +3,24 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		if(a + b == c) bw.write(a + "+" + b + "=" + c);
-		else if(a - b == c) bw.write(a + "-" + b + "=" + c);
-		else if(a * b == c) bw.write(a + "*" + b + "=" + c);
-		else if(a / b == c) bw.write(a + "/" + b + "=" + c);
-		else if(b + c == a) bw.write(a + "=" + b + "+" + c);
-		else if(b - c == a) bw.write(a + "=" + b + "-" + c);
-		else if(b * c == a) bw.write(a + "=" + b + "*" + c);
-		else if(b / c == a) bw.write(a + "=" + b + "/" + c);
+		String binary = br.readLine();
+		if(binary.length() % 3 == 1) binary = "00"+binary;
+		else if(binary.length() % 3 == 2) binary = "0"+binary;
+		for(int i = 0; i<binary.length(); i+=3) {
+			char[] tmp = binary.substring(i, i+3).toCharArray();
+			int flag = 4;
+			int val = 0;
+			for(int j = 0; j<3; j++) {
+				val += (tmp[j]-'0') * flag;
+				flag /= 2;
+			}
+			bw.write(val+"");
+		}
 		bw.flush();
 		bw.close();
 		br.close();
