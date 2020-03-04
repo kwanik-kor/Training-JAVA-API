90e1f88d6c6268afa8e3f0c680a3266743a0d6f2
kwanik-kor <kwanigi2005@gmail.com>
2020-03-04 19:19:47 +0900

    20200304 19:19 백준알고리즘/3034/앵그리창영

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -10,33 +10,13 @@ public class TempForEasyOne {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int R = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		char card[][] = new char[2*R][2*C];
-		for(int i = 0; i<R; i++) {
-			String tmp = br.readLine();
-			for(int j = 0; j<C; j++)
-				card[i][j] = tmp.charAt(j);
-		}
-		for(int i = 0; i<R; i++) {
-			for(int j = C; j<2*C; j++)
-				card[i][j] = card[i][2*C - j - 1];
-		}
-		for(int i = R; i<2*R; i++) {
-			for(int j = 0; j<2*C; j++)
-				card[i][j] = card[2*R - i -1][j];
-		}
-		st = new StringTokenizer(br.readLine());
-		int A = Integer.parseInt(st.nextToken()) - 1;
-		int B = Integer.parseInt(st.nextToken()) - 1;
-		for(int i = 0; i<2*R; i++) {
-			for(int j = 0; j<2*C; j++) {
-				if(i == A && j == B) {
-					if(card[i][j] == '#') bw.write(".");
-					else bw.write("#");
-				}else bw.write(card[i][j] + "");
-			}
-			bw.write("\n");
+		int N = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int H = Integer.parseInt(st.nextToken());
+		double tmp = Math.sqrt(W*W + H*H);
+		for(int i = 0; i<N; i++) {
+			if(Integer.parseInt(br.readLine()) <= tmp) bw.write("DA\n");
+			else bw.write("NE\n");
 		}
 		bw.flush();
 		bw.close();
