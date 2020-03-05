c7965e9ebbd10c35e32a5990b9348c5a872c2c72
kwanik-kor <kwanigi2005@gmail.com>
2020-03-05 09:44:55 +0900

    20200305 09:45 백준알고리즘/4740/오 거울 오거울

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -9,11 +9,11 @@ public class TempForEasyOne {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		String s= "";
-		while(!(s = br.readLine()).equals("#")) {
-			int sum = 0;
+		while(!(s = br.readLine()).equals("***")) {
+			String tmp = "";
 			for(int i = 0; i<s.length(); i++) 
-				sum += (i+1) *((s.charAt(i) == ' ')? 0 : s.charAt(i)-'A'+1);
-			bw.write(sum + "\n");
+				tmp += s.charAt(s.length() - 1 - i);
+			bw.write(tmp + "\n");
 		}
 		bw.flush();
 		bw.close();
