a74411e84eec036b7ebf9e74e03b59517ea75d63
kwanik-kor <kwanigi2005@gmail.com>
2020-03-06 12:19:41 +0900

    20200306 12:19 백준알고리즘/1302/베스트셀러

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -3,26 +3,32 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 
 public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int testCase = Integer.parseInt(br.readLine());
-		BigInteger arr[] = new BigInteger[68];
-		arr[0] = arr[1] = BigInteger.valueOf(1);
-		arr[2] = BigInteger.valueOf(2);
-		arr[3] = BigInteger.valueOf(4);
-		for(int i = 4; i<=67; i++) {
-			BigInteger a = arr[i-1];
-			BigInteger b = arr[i-2];
-			BigInteger c = arr[i-3];
-			BigInteger d = arr[i-4];
-			arr[i] = a.add(b.add(c.add(d)));
+		int N = Integer.parseInt(br.readLine());
+		HashMap<String, Integer> map = new HashMap<>();
+		for(int i = 0; i<N; i++) {
+			String title = br.readLine();
+			if(map.containsKey(title)) map.replace(title, map.get(title) + 1);
+			else map.put(title, 1);
 		}
-		for(int t = 1; t<=testCase; t++) 
-			bw.write(arr[Integer.parseInt(br.readLine())].toString() + "\n");
+		List<String> keyList = new ArrayList<>(map.keySet());
+		keyList.sort(new Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				if(map.get(s1).compareTo(map.get(s2)) == 0)
+					return s1.compareTo(s2);
+				return map.get(s2).compareTo(map.get(s1));
+			}
+		});
+		bw.write(keyList.get(0));
 		bw.flush();
 		bw.close();
 		br.close();
