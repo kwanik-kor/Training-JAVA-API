18be7f77f915763db309f8a91ef488c33aede441
kwanik-kor <kwanigi2005@gmail.com>
2020-03-07 10:33:07 +0900

    20200307 10:32 백준알고리즘/10769/행복한지 슬픈지

--- StudyExamples/src/TempForEasyOne.java
+++ StudyExamples/src/TempForEasyOne.java
@@ -8,11 +8,17 @@ public class TempForEasyOne {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int a = Integer.parseInt(br.readLine());
-		int b = Integer.parseInt(br.readLine());
-		if(a == 2 && b == 18) bw.write("Special");
-		else if(a < 2 || a == 2 && b < 18) bw.write("Before");
-		else if(a > 2 || a == 2 && b > 18) bw.write("After");
+		String s = br.readLine();
+		int happy = 0;
+		int sad = 0;
+		for(int i = 0; i<s.length() - 2; i++) {
+			if(s.substring(i, i+3).equals(":-)")) happy++;
+			else if(s.substring(i, i+3).equals(":-(")) sad++;
+		}
+		if(happy == 0 && sad == 0) bw.write("none");
+		else if(happy == sad) bw.write("unsure");
+		else if(happy > sad) bw.write("happy");
+		else if(happy < sad) bw.write("sad");
 		bw.flush();
 		bw.close();
 		br.close();
