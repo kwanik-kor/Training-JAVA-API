7c60e4c97a4daf7f94f71df022c0dbe8f7d883e0
kwanik-kor <rhksdlr134@naver.com>
2020-03-15 18:16:45 +0900

    20200315 18:16 백준알고리즘/14502/연구소

--- StudyExamples/src/baekjun/bruteforce/Retirement14501.java
+++ StudyExamples/src/baekjun/bruteforce/Retirement14501.java
@@ -8,42 +8,34 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Retirement14501 {
-	static int N, max;
-	static Council[] schedule;
+	static int max = 0;
+	static int schedule[][];
 	
-	static void maxBenefit(int idx, int sum) {
-		if(idx == N) {
+	static void maxBenefit(int n, int idx, int sum) {
+		if(idx == n) {
 			max = Math.max(max, sum);
 			return;
 		}
-		if(idx + schedule[idx].day <= N) {
-			maxBenefit(idx + schedule[idx].day, sum + schedule[idx].val);
+		if(idx + schedule[idx][0] <= n) {
+			maxBenefit(n, idx + schedule[idx][0], sum + schedule[idx][1]);
 		}
-		maxBenefit(idx + 1, sum);
+		maxBenefit(n, idx + 1, sum);
 	}
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		N = Integer.parseInt(br.readLine());
-		schedule = new Council[N];
-		for(int i = 0; i<N; i++) {
+		int n = Integer.parseInt(br.readLine());
+		schedule = new int[n][2];
+		for(int i = 0; i<n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			schedule[i] = new Council(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			schedule[i][0] = Integer.parseInt(st.nextToken());
+			schedule[i][1] = Integer.parseInt(st.nextToken());
 		}
-		maxBenefit(0, 0);
+		maxBenefit(n, 0, 0);
 		bw.write(max + "");
 		bw.flush();
 		bw.close();
 		br.close();
-	}
-	
-	static class Council{
-		int day;
-		int val;
-		public Council(int day, int val) {
-			this.day = day;
-			this.val = val;
-		}
 	}
 }
