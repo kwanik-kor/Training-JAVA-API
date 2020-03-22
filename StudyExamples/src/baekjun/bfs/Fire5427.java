23f797116a4d2818fdff6bcbcf30d21d624dbdb0
kwanik-kor <rhksdlr134@naver.com>
2020-03-22 14:48:44 +0900

    20200322 14:50 백준알고리즘/5427/불

--- StudyExamples/src/baekjun/bfs/Fire5427.java
+++ StudyExamples/src/baekjun/bfs/Fire5427.java
@@ -5,62 +5,59 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Fire5427 {
-	static int w, h;
-	static int EMPTY = 0;
-	static int MAN = 1;
-	static int FIRE = 2;
-	static char map[][] = new char[1001][1001];
-	static int dy[] = {1, 0, -1, 0};
-	static int dx[] = {0, 1, 0, -1};
-	static Point SangGeun;
-	static Point[][] state;
-	static ArrayList<Point> fire;
-	static int minEscape;
+	static int h, w;
+	static char[][] map;
+	static Queue<Point> fire, man;
+	static int[] dy = {0, 1, 0, -1};
+	static int[] dx = {1, 0, -1, 0};
 	static int INF = 987654321;
 	
-	static void bfs() {
-		state = new Point[h][w];
-		for(int i = 0; i<h; i++) {
-			for(int j = 0; j<w; j++)
-				state[i][j] = new Point();
-		}
-		
-		Queue<Point> q = new LinkedList<Point>();
-		state[SangGeun.y][SangGeun.x] = new Point(0, MAN);
-		q.add(SangGeun);
-		for(Point p : fire) {
-			state[p.y][p.x] = new Point(0, FIRE);
-			q.add(p);
-		}
-		
-		while(!q.isEmpty()) {
-			Point cur = q.poll();
-			int curtime = state[cur.y][cur.x].y;
-			int curstate = state[cur.y][cur.x].x;
-			for(int i = 0; i<4; i++) {
-				int ny = cur.y + dy[i];
-				int nx = cur.x + dx[i];
-				if(ny < 0 || nx < 0 || ny >= h || nx >= w) {
-					if(curstate == MAN)
-						minEscape = Math.min(minEscape, curtime + 1);
-					continue;
+	static int bfs() {
+		int cnt = 0;
+		while(true) {
+			cnt++;
+			int mansz = man.size();
+			if(mansz == 0) {
+				cnt = INF;
+				break;
+			}
+			for(int i = 0; i<mansz; i++) {
+				Point cur = man.poll();
+				for(int dir = 0; dir<4; dir++) {
+					int ny = cur.y + dy[dir];
+					int nx = cur.x + dx[dir];
+					if(ny < 0 || nx < 0 || ny >= h || nx >= w) 
+						return cnt;
+					else {
+						if(map[ny][nx] == '.') {
+							map[ny][nx] = '@';
+							man.add(new Point(ny, nx));
+						}
+					}
 				}
-				if(map[ny][nx] == '#') continue;
-				if(state[ny][nx].x == FIRE) continue;
-				if(state[ny][nx].x == EMPTY || state[ny][nx].x == MAN) {
-					state[ny][nx] = new Point(curtime + 1, curstate);
-					q.add(new Point(ny, nx));
+			}
+			
+			int firesz = fire.size();
+			for(int i = 0; i<firesz; i++) {
+				Point cur = fire.poll();
+				for(int dir = 0; dir<4; dir++) {
+					int ny = cur.y + dy[dir];
+					int nx = cur.x + dx[dir];
+					if(ny >= 0 && nx >= 0 && ny < h && nx < w && map[ny][nx] != '#') {
+						map[ny][nx] = '*';
+						fire.add(new Point(ny, nx));
+					}
 				}
 			}
 		}
+		return cnt;
 	}
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -69,21 +66,23 @@ public class Fire5427 {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
-			minEscape = INF;
-			fire = new ArrayList<Point>();
+			map = new char[h][w];
+			man = new LinkedList<Point>();
+			fire = new LinkedList<Point>();
 			for(int i = 0; i<h; i++) {
 				String tmp = br.readLine();
 				for(int j = 0; j<w; j++) {
 					map[i][j] = tmp.charAt(j);
-					if(map[i][j] == '@')
-						SangGeun = new Point(i, j);
-					else if(map[i][j] == '*')
+					if(map[i][j] == '*')
 						fire.add(new Point(i, j));
+					if(map[i][j] == '@')
+						man.add(new Point(i, j));
 				}
 			}
-			bfs();
-			bw.write((minEscape == 987654321)? "IMPOSSIBLE\n" : minEscape + "\n");
+			int ret = bfs();
+			bw.write((ret == INF)? "IMPOSSIBLE\n" : ret + "\n");
 		}
+		
 		bw.flush();
 		bw.close();
 		br.close();
@@ -92,10 +91,6 @@ public class Fire5427 {
 	static class Point{
 		int y;
 		int x;
-		public Point() {
-			this.y = 0;
-			this.x = 0;
-		}
 		public Point(int y, int x) {
 			this.y = y;
 			this.x = x;
