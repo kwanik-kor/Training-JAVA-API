b361a0a97b3a85cb9ff3bfa3797bda3d4826bd2e
kwanik-kor <rhksdlr134@naver.com>
2020-03-22 15:20:37 +0900

    20200322 15:20 백준알고리즘/5427/불

--- StudyExamples/src/baekjun/bfs/Fire5427.java
+++ StudyExamples/src/baekjun/bfs/Fire5427.java
@@ -11,53 +11,76 @@ import java.util.StringTokenizer;
 
 public class Fire5427 {
 	static int h, w;
-	static char[][] map;
-	static Queue<Point> fire, man;
+	static int result;
+	static int[][] map;
+	static Queue<int[]> fire, man;
 	static int[] dy = {0, 1, 0, -1};
 	static int[] dx = {1, 0, -1, 0};
-	static int INF = 987654321;
 	
-	static int bfs() {
-		int cnt = 0;
-		while(true) {
-			cnt++;
-			int mansz = man.size();
-			if(mansz == 0) {
-				cnt = INF;
-				break;
-			}
-			for(int i = 0; i<mansz; i++) {
-				Point cur = man.poll();
-				for(int dir = 0; dir<4; dir++) {
-					int ny = cur.y + dy[dir];
-					int nx = cur.x + dx[dir];
-					if(ny < 0 || nx < 0 || ny >= h || nx >= w) 
-						return cnt;
-					else {
-						if(map[ny][nx] == '.') {
-							map[ny][nx] = '@';
-							man.add(new Point(ny, nx));
-						}
+	static void bfs() {
+		while(!fire.isEmpty() || !man.isEmpty()) {
+			int size1 = fire.size();
+			if(size1 > 0) {
+				while(size1-- > 0) {
+					int y = fire.peek()[0];
+					int x = fire.peek()[1];
+					fire.poll();
+					
+					for(int dir = 0; dir<4; dir++) {
+						int ny = y + dy[dir];
+						int nx = x + dx[dir];
+						
+						if(ny < 0 || nx < 0 || ny >= h || nx >= w) continue;
+						if(map[ny][nx] == -1 || map[ny][nx] == -2) continue;
+						
+						map[ny][nx] = -2;
+						fire.add(new int[] {ny, nx});
 					}
 				}
 			}
 			
-			int firesz = fire.size();
-			for(int i = 0; i<firesz; i++) {
-				Point cur = fire.poll();
-				for(int dir = 0; dir<4; dir++) {
-					int ny = cur.y + dy[dir];
-					int nx = cur.x + dx[dir];
-					if(ny >= 0 && nx >= 0 && ny < h && nx < w && map[ny][nx] != '#') {
-						map[ny][nx] = '*';
-						fire.add(new Point(ny, nx));
+			int size2 = man.size();
+			boolean flag = false;
+			if(size2 > 0 ) {
+				while(size2-- > 0) {
+					int y = man.peek()[0];
+					int x = man.peek()[1];
+					int val = man.peek()[2];
+					man.poll();
+					
+					for(int dir = 0; dir<4; dir++) {
+						int ny = y + dy[dir];
+						int nx = x + dx[dir];
+						if(ny < 0 || ny >= h || nx < 0 || nx >= w) {
+							result = val + 1;
+							flag = true;
+							break;
+						}
+						if(map[ny][nx] == 0) {
+							map[ny][nx] = val + 1;
+							man.offer(new int[] {ny, nx, val + 1});
+						}
 					}
+					
+					if(flag) break;
 				}
+				if(flag) break;
+			}else {
+				result = -1;
+				break;
 			}
 		}
-		return cnt;
 	}
 	
+    static int getVal(char c) {
+        switch(c) {
+            case '#': return -1;
+            case '*': return -2;
+            case '@': return 1; 
+            default: return 0;
+        }
+    }
+    
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -66,34 +89,28 @@ public class Fire5427 {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
-			map = new char[h][w];
-			man = new LinkedList<Point>();
-			fire = new LinkedList<Point>();
+			map = new int[h][w];
+			man = new LinkedList<>();
+			fire = new LinkedList<>();
+			result = 0;
+			
 			for(int i = 0; i<h; i++) {
 				String tmp = br.readLine();
 				for(int j = 0; j<w; j++) {
-					map[i][j] = tmp.charAt(j);
-					if(map[i][j] == '*')
-						fire.add(new Point(i, j));
-					if(map[i][j] == '@')
-						man.add(new Point(i, j));
+					map[i][j] = getVal(tmp.charAt(j));
+					if(map[i][j] == -2)
+						fire.offer(new int[] {i, j});
+					if(map[i][j] == 1)
+						man.offer(new int[] {i, j, 0});
 				}
 			}
-			int ret = bfs();
-			bw.write((ret == INF)? "IMPOSSIBLE\n" : ret + "\n");
+			bfs();
+			bw.write((result > 0)? result + "\n" : "IMPOSSIBLE\n");
 		}
 		
 		bw.flush();
 		bw.close();
 		br.close();
 	}
-	
-	static class Point{
-		int y;
-		int x;
-		public Point(int y, int x) {
-			this.y = y;
-			this.x = x;
-		}
-	}
+
 }
